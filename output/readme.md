7224cea7b4e5e9d4c3cc094d23263feb52e6d1f7
Nate Johnson <njohnson2897@gmail.com>
2024-03-21 18:37:41 -0500

    formats markdown generator more and adds renderLicenseSection functionality

--- output/readme.md
+++ output/readme.md
@@ -1,11 +1,26 @@
 # Readme Generator 
+
+  ## Table of Contents:
+    [Description](#Description)
+    [Installation](#Installation)
+    [Usage](#Usage)
+    [Contributing](#Contributing)
+    [Tests](#Tests)
+    [Questions](#Questions)
+
   ## Description: It is a readme generator
-  ## Installation: It is a readme generator
-  ## Usage: It is a readme generator
-  ## Contributing: It is a readme generator
-  ## Tests: It is a readme generator
-  ## License: It is a readme generator
+
+  ## Installation: npm install
+
+  ## Usage: To generate readme files
+
+  ## Contributing: No one
+
+  ## Tests: npm test
+
   ## Questions?
   njohnson2897@gmail.com
   njohnson2897
+
+  ## License:
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,24 +4,53 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  // if (license) {
+  //   return `\n${}`
+  // }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License:`
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title} 
-  ## Description: ${data.description}
-  ## Installation: ${data.description}
-  ## Usage: ${data.description}
-  ## Contributing: ${data.description}
-  ## Tests: ${data.description}
-  ## License: ${data.description}
+
+  ## Table of Contents
+    [Description](#Description)
+    [Installation](#Installation)
+    [Usage](#Usage)
+    [Contributing](#Contributing)
+    [Tests](#Tests)
+    [Questions](#Questions)
+
+  ## Description 
+  ${data.description}
+
+  ## Installation 
+  ${data.installation}
+
+  ## Usage 
+  ${data.usage}
+
+  ## Contributing 
+  ${data.contributing}
+
+  ## Tests 
+  ${data.tests}
+
   ## Questions?
   ${data.email}
   ${data.github}
+
+  ${renderLicenseSection(data.license)}
 
 `;
 }
